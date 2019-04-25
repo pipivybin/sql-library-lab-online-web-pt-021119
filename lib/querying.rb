19899f5b82eea13fa81bb813fa029c3d1358bf09
Ming Duan <11203duan@gmail.com>
2019-04-25 01:12:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subseries
+  FROM authors
+  INNER JOIN series
+  ON authors.id = series.author_id
+  GROUP BY authors.name"
 end
 
 def select_series_title_with_most_human_characters
