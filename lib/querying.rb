1fabb7c08c52d29b52ba28acdee6850bdcbc5257
Ming Duan <11203duan@gmail.com>
2019-04-25 00:58:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year
+  "SELECT books.title, year
   FROM books
   INNER JOIN series
   ON books.series_id = series.id
