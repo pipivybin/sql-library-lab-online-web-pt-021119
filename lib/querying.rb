20204db9151f6057cfd8a6ff78eb7059ae26b633
Ming Duan <11203duan@gmail.com>
2019-04-25 01:21:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,6 +39,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON series.id = characters.series_id
   WHERE species = 'human'
+  GROUP BY series.title
   ORDER BY SUM(species) DESC
   LIMIT 1;"
 end
