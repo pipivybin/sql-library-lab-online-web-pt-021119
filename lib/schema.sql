6de2897a191dd89378b4ed15a5001989c50bb024
Ming Duan <11203duan@gmail.com>
2019-04-25 00:42:43 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
