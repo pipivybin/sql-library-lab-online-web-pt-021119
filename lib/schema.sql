5765dcaf3be63889ba4600c94d63ccbc03a02dc0
Ming Duan <11203duan@gmail.com>
2019-04-25 00:45:19 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -21,3 +21,12 @@ CREATE TABLE books (
   series_id INTEGER,
   year TEXT
 );
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  series_id INTEGER,
+  author_id INTEGER
+);
